344a1bfb438c039c664ecea85f1cf10a155aa206
sighlmao <samgiabao2018@gmail.com>
2024-06-02 23:42:51 +0700

    sam1

---
+++ app/src/main/java/com/example/firebase2/MyApplication.java
@@ -0,0 +1,13 @@
+package com.example.firebase2;
+
+import android.app.Application;
+
+import com.google.firebase.database.FirebaseDatabase;
+public class MyApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        // Initialize Firebase
+        FirebaseDatabase.getInstance().setPersistenceEnabled(true);
+    }
+}
